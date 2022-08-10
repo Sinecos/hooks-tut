6abefdd391ed5624c55672d4c1ae70f045e4e030
Theal Altake <zufa.altake@gmail.com>
2022-08-11 01:59:04 +0300

    useState finish

---
+++ src/UseReducer.js
@@ -0,0 +1,7 @@
+import React, { useReducer } from "react";
+
+const UseReducer = () => {
+    
+};
+
+export default UseReducer;
--- src/UseState.js
+++ src/UseState.js
@@ -1,19 +1,36 @@
 import React, { useState } from "react";
 
+
 const Usestate = () => {
 
-    const [counter, setCounter] = useState(0);
+    const [inputValue, setInputValue] = useState("Zufa");
 
-    const increment = () => {
-        setCounter(counter + 1);
+    let onChange = (event) => {
+        const newValue = event.target.value;
+        setInputValue(newValue);
     };
 
-    return(
+    return (
         <div>
-            {counter} 
-            <button onClick={increment}>Increment</button>
+            <input placeholder="enter something..." onChange={onChange} />
+            {inputValue}
         </div>
+
     );
+
+    //// Using useState by incrementing number
+    // const [counter, setCounter] = useState(0);
+
+    // const increment = () => {
+    //     setCounter(counter + 1);
+    // };
+
+    // return(
+    //     <div>
+    //         {counter} 
+    //         <button onClick={increment}>Increment</button>
+    //     </div>
+    // );
 };
 
 export default Usestate;
