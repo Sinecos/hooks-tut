ea4dd223faf6fa5fb94ddef368394282c714f539
Theal Altake <zufa.altake@gmail.com>
2022-08-10 22:03:09 +0300

    init

---
+++ src/UseState.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+
+const Usestate = () => {
+
+    const [counter, setCounter] = useState(0);
+
+    const increment = () => {
+        setCounter(counter + 1);
+    };
+
+    return(
+        <div>
+            {counter} 
+            <button onClick={increment}>Increment</button>
+        </div>
+    );
+};
+
+export default Usestate;
